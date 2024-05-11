c9225f2e044e997f775713e45df03a55a55f62f6
theanxiuser <theanxiuser@gmail.com>
2024-05-11 13:36:08 +0545

    feat: handle the errors

--- source_code/program/__main__.py
+++ source_code/program/__main__.py
@@ -1,12 +1,27 @@
 import os
 import sys
+import netifaces
 
 from packet_capture import PacketCapture
 from packet_analyzer import PacketAnalyzer
 from packet_handler import PacketHandler
 
 
+def list_network_interfaces():
+    interfaces = netifaces.interfaces()
+    print("Available network interfaces:")
+    for interface in interfaces:
+        print(f"---------> {interface}")
+
+
 def main():
+    list_network_interfaces()
+    try:
+        chosen_interface = input("Enter the name of the network interface to use: ")
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sys.exit(0)
+
     packet_handler = PacketHandler()
     packet_analyzer = PacketAnalyzer(packet_handler)
     packet = PacketCapture(packet_analyzer)
@@ -15,6 +30,6 @@ def main():
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
-        print("Error: This program must be run with superuser privileges.")
+        print("Error: You cannot perform this operation unless you are root.")
         sys.exit(1)
     main()
--- source_code/program/packet_capture.py
+++ source_code/program/packet_capture.py
@@ -2,12 +2,22 @@ import socket
 
 
 class PacketCapture:
-    def __init__(self, packet_analyzer):
+    def __init__(self, packet_analyzer, interface="wlp2s0"):
         self.packet_analyzer = packet_analyzer
+        self.interface = interface
 
     def start_capture(self):
         with socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3)) as raw_socket:
-            raw_socket.bind(("wlp2s0", 0))
+            raw_socket.bind((self.interface, 0))
             while True:
-                raw_data = raw_socket.recvfrom(65536)
-                self.packet_analyzer.analyze(raw_data)
+                try:
+                    raw_data = raw_socket.recvfrom(65536)
+                    self.packet_analyzer.analyze(raw_data)
+                except KeyboardInterrupt:
+                    print("\nTerminated by user.")
+                    print("Exiting...")
+                    break
+                except Exception as e:
+                    print("\nAn error occurred.")
+                    print("Exiting...")
+                    break
