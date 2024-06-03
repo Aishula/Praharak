6f6f3f497ca60838075b4e120a5c9b21d2d10648
theanxiuser <theanxiuser@gmail.com>
2024-06-03 09:57:35 +0545

    fix: direction and reduced fts

--- packet_analyzer/flow/flow.py
+++ packet_analyzer/flow/flow.py
@@ -215,12 +215,12 @@ class Flow:
 
         data = {
             # Basic IP information
-            "src_ip": self.src_ip,
-            "dst_ip": self.dst_ip,
-            "src_port": self.src_port,
-            "dst_port": self.dst_port,
-            "src_mac": self.src_mac,
-            "dst_mac": self.dst_mac,
+            # "src_ip": self.src_ip,
+            # "dst_ip": self.dst_ip,
+            # "src_port": self.src_port,
+            # "dst_port": self.dst_port,
+            # "src_mac": self.src_mac,
+            # "dst_mac": self.dst_mac,
             "protocol": self.protocol,
 
             # Basic information from packet times
@@ -228,7 +228,7 @@ class Flow:
             "flow_duration": 1e6 * packet_time.get_duration(),
             "flow_byts_s": flow_bytes.get_rate(),
             "flow_pkts_s": packet_count.get_rate(),
-            "fwd_pkts_s": packet_count.get_rate(PacketDirection.FORWARD),
+            # "fwd_pkts_s": packet_count.get_rate(PacketDirection.FORWARD),
             "bwd_pkts_s": packet_count.get_rate(PacketDirection.REVERSE),
 
             # Count total packets by direction
@@ -247,8 +247,8 @@ class Flow:
             "bwd_pkt_len_mean": float(packet_length.get_mean(PacketDirection.REVERSE)),
             "bwd_pkt_len_std": float(packet_length.get_std(PacketDirection.REVERSE)),
             "pkt_len_max": packet_length.get_max(),
-            "pkt_len_min": packet_length.get_min(),
-            "pkt_len_mean": float(packet_length.get_mean()),
+            # "pkt_len_min": packet_length.get_min(),
+            # "pkt_len_mean": float(packet_length.get_mean()),
             "pkt_len_std": float(packet_length.get_std()),
             "pkt_len_var": float(packet_length.get_var()),
             "fwd_header_len": flow_bytes.get_forward_header_bytes(),
@@ -261,61 +261,61 @@ class Flow:
             "flow_iat_max": float(flow_iat["max"]),
             "flow_iat_min": float(flow_iat["min"]),
             "flow_iat_std": float(flow_iat["std"]),
-            "fwd_iat_tot": forward_iat["total"],
-            "fwd_iat_max": float(forward_iat["max"]),
+            # "fwd_iat_tot": forward_iat["total"],
+            # "fwd_iat_max": float(forward_iat["max"]),
             "fwd_iat_min": float(forward_iat["min"]),
-            "fwd_iat_mean": float(forward_iat["mean"]),
-            "fwd_iat_std": float(forward_iat["std"]),
+            # "fwd_iat_mean": float(forward_iat["mean"]),
+            # "fwd_iat_std": float(forward_iat["std"]),
             "bwd_iat_tot": float(backward_iat["total"]),
-            "bwd_iat_max": float(backward_iat["max"]),
+            # "bwd_iat_max": float(backward_iat["max"]),
             "bwd_iat_min": float(backward_iat["min"]),
             "bwd_iat_mean": float(backward_iat["mean"]),
-            "bwd_iat_std": float(backward_iat["std"]),
+            # "bwd_iat_std": float(backward_iat["std"]),
 
             # Flags statistics
             "fwd_psh_flags": flag_count.has_flag("PSH", PacketDirection.FORWARD),
-            "bwd_psh_flags": flag_count.has_flag("PSH", PacketDirection.REVERSE),
+            # "bwd_psh_flags": flag_count.has_flag("PSH", PacketDirection.REVERSE),
             "fwd_urg_flags": flag_count.has_flag("URG", PacketDirection.FORWARD),
-            "bwd_urg_flags": flag_count.has_flag("URG", PacketDirection.REVERSE),
-            "fin_flag_cnt": flag_count.has_flag("FIN"),
+            # "bwd_urg_flags": flag_count.has_flag("URG", PacketDirection.REVERSE),
+            # "fin_flag_cnt": flag_count.has_flag("FIN"),
             "syn_flag_cnt": flag_count.has_flag("SYN"),
             "rst_flag_cnt": flag_count.has_flag("RST"),
-            "psh_flag_cnt": flag_count.has_flag("PSH"),
-            "ack_flag_cnt": flag_count.has_flag("ACK"),
-            "urg_flag_cnt": flag_count.has_flag("URG"),
-            "ece_flag_cnt": flag_count.has_flag("ECE"),
+            # "psh_flag_cnt": flag_count.has_flag("PSH"),
+            # "ack_flag_cnt": flag_count.has_flag("ACK"),
+            # "urg_flag_cnt": flag_count.has_flag("URG"),
+            # "ece_flag_cnt": flag_count.has_flag("ECE"),
 
             # Response Time
-            # "down_up_ratio": packet_count.get_down_up_ratio(),
-            # "pkt_size_avg": packet_length.get_avg(),
+            "down_up_ratio": packet_count.get_down_up_ratio(),
+            "pkt_size_avg": packet_length.get_avg(),
             "init_fwd_win_byts": self.init_window_size[PacketDirection.FORWARD],
             "init_bwd_win_byts": self.init_window_size[PacketDirection.REVERSE],
-            "active_max": float(active_stat["max"]),
-            "active_min": float(active_stat["min"]),
-            "active_mean": float(active_stat["mean"]),
-            "active_std": float(active_stat["std"]),
-            "idle_max": float(idle_stat["max"]),
-            "idle_min": float(idle_stat["min"]),
+            # "active_max": float(active_stat["max"]),
+            # "active_min": float(active_stat["min"]),
+            # "active_mean": float(active_stat["mean"]),
+            # "active_std": float(active_stat["std"]),
+            # "idle_max": float(idle_stat["max"]),
+            # "idle_min": float(idle_stat["min"]),
             "idle_mean": float(idle_stat["mean"]),
             "idle_std": float(idle_stat["std"]),
-            "fwd_byts_b_avg": float(
-                flow_bytes.get_bytes_per_bulk(PacketDirection.FORWARD)
-            ),
-            "fwd_pkts_b_avg": float(
-                flow_bytes.get_packets_per_bulk(PacketDirection.FORWARD)
-            ),
-            "bwd_byts_b_avg": float(
-                flow_bytes.get_bytes_per_bulk(PacketDirection.REVERSE)
-            ),
-            "bwd_pkts_b_avg": float(
-                flow_bytes.get_packets_per_bulk(PacketDirection.REVERSE)
-            ),
-            "fwd_blk_rate_avg": float(
-                flow_bytes.get_bulk_rate(PacketDirection.FORWARD)
-            ),
-            "bwd_blk_rate_avg": float(
-                flow_bytes.get_bulk_rate(PacketDirection.REVERSE)
-            ),
+            # "fwd_byts_b_avg": float(
+            #     flow_bytes.get_bytes_per_bulk(PacketDirection.FORWARD)
+            # ),
+            # "fwd_pkts_b_avg": float(
+            #     flow_bytes.get_packets_per_bulk(PacketDirection.FORWARD)
+            # ),
+            # "bwd_byts_b_avg": float(
+            #     flow_bytes.get_bytes_per_bulk(PacketDirection.REVERSE)
+            # ),
+            # "bwd_pkts_b_avg": float(
+            #     flow_bytes.get_packets_per_bulk(PacketDirection.REVERSE)
+            # ),
+            # "fwd_blk_rate_avg": float(
+            #     flow_bytes.get_bulk_rate(PacketDirection.FORWARD)
+            # ),
+            # "bwd_blk_rate_avg": float(
+            #     flow_bytes.get_bulk_rate(PacketDirection.REVERSE)
+            # ),
         }
 
         # Duplicated features
@@ -324,8 +324,8 @@ class Flow:
         data["cwe_flag_count"] = data["fwd_urg_flags"]
         data["subflow_fwd_pkts"] = data["tot_fwd_pkts"]
         data["subflow_bwd_pkts"] = data["tot_bwd_pkts"]
-        data["subflow_fwd_byts"] = data["totlen_fwd_pkts"]
-        data["subflow_bwd_byts"] = data["totlen_bwd_pkts"]
+        # data["subflow_fwd_byts"] = data["totlen_fwd_pkts"]
+        # data["subflow_bwd_byts"] = data["totlen_bwd_pkts"]
 
         return data
 
--- packet_analyzer/flow_session.py
+++ packet_analyzer/flow_session.py
@@ -13,7 +13,7 @@ class FlowSession:
         self.flows = {}
         self.expiration_threshold = 40
         self.expiry_duration = 60
-        self.garbage_collection_threshold = 50  # After 100 packet processed
+        self.garbage_collection_threshold = 100  # After 100 packet processed
         self.packet_count = 0
 
     def process_packet(self, raw_data, timestamp):
@@ -29,7 +29,33 @@ class FlowSession:
                 packet.src_port is None or packet.dst_port is None):
             return
 
-        direction = self._get_packet_direction(packet)
+        print("Dir from fun call", self._get_packet_direction(packet, count))
+
+        # Consider the direction is forward
+        # direction = PacketDirection.FORWARD
+        # # check if the flow exists
+        # flow_key = packet_flow_key.get_packet_flow_key(packet, direction)
+        # flow = self.flows.get((flow_key, count))
+        # print("fwd (first test)", flow)
+
+        # self.packet_count += 1
+        #
+        # # If there is no existing forward flow, then there might be reverse with count 0
+        # if flow is None:
+        #     direction = PacketDirection.REVERSE
+        #     flow_key = packet_flow_key.get_packet_flow_key(packet, direction)
+        #     flow = self.flows.get((flow_key, count))
+        #     # print("bwd (first test)", flow)
+
+        # If no flow exists in above case, create new fwd
+        # if flow is None:
+        #     direction = PacketDirection.FORWARD
+        #     flow = Flow(packet, direction)
+        #     flow_key = packet_flow_key.get_packet_flow_key(packet, direction)
+        #     self.flows[(flow_key, count)] = flow
+        #     # print("fwd (new obj)", flow)
+
+        direction = self._get_packet_direction(packet, count)
         flow_key = packet_flow_key.get_packet_flow_key(packet, direction)
         print("FLOW ID: ", flow_key)
         # print(self.flows.values())
@@ -44,6 +70,7 @@ class FlowSession:
             flow = Flow(packet, direction)
             self.flows[(flow_key, count)] = flow
 
+        # Flow for a packet is found but time is expired
         elif packet.timestamp - flow.latest_timestamp > self.expiration_threshold:
             expired = self.expiration_threshold
             while packet.timestamp - flow.latest_timestamp > expired:
@@ -52,8 +79,10 @@ class FlowSession:
                 flow = self.flows.get((flow_key, count))
 
                 if flow is None:
+                    direction = PacketDirection.FORWARD
                     flow = Flow(packet, direction)
                     self.flows[(flow_key, count)] = flow
+                    # print("fwd (time expired)", flow)
                     break
 
         elif "F" in str(packet.flags):
@@ -61,8 +90,12 @@ class FlowSession:
             self._perform_garbage_collection(packet.timestamp)
             return
 
+        # print("Dir from Calc", direction)
+        # print("---------------------")
+        # Finally add packet to the flow
         flow.add_packet(packet, direction)
 
+        # Pass the flow to garbage collection
         if self.packet_count % self.garbage_collection_threshold == 0 or (
                 flow.duration > self.expiry_duration):
             self._perform_garbage_collection(packet.timestamp)
@@ -89,7 +122,7 @@ class FlowSession:
                 print(len(data))
                 del self.flows[k]
 
-    def _get_packet_direction(self, packet):
+    def _get_packet_direction(self, packet, count):
         """
         Determine the direction of a packet, either forward or backward
         :param packet:
@@ -97,10 +130,10 @@ class FlowSession:
         """
         flow_key_forward = packet_flow_key.get_packet_flow_key(packet, PacketDirection.FORWARD)
         flow_key_backward = packet_flow_key.get_packet_flow_key(packet, PacketDirection.REVERSE)
-        if flow_key_forward in self.flows:
+        if (flow_key_forward, count) in self.flows:
             return PacketDirection.FORWARD
 
-        elif flow_key_backward in self.flows:
+        elif (flow_key_backward, count) in self.flows:
             return PacketDirection.REVERSE
         else:
             return PacketDirection.FORWARD  # First packet in a new flow
