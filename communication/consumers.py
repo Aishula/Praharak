84fdd1d8aba49aebba0b73f715379cb4ef493c67
theanxiuser <theanxiuser@gmail.com>
2024-06-24 09:00:59 +0545

    commn prototype done

--- communication/consumers.py
+++ communication/consumers.py
@@ -4,16 +4,17 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class PacketFlowDataConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        print("connected")
         await self.accept()
 
     async def disconnect(self, close_code):
         pass
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
 
--- communication/routing.py
+++ communication/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/flow_data/', consumers.PacketFlowDataConsumer.as_asgi()),
+    path('ws/ws-test/', consumers.PacketFlowDataConsumer.as_asgi()),
 ]
