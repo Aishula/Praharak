c33311b3bf9443693a25a1e1a809b1e6bf22cf6c
theanxiuser <theanxiuser@gmail.com>
2024-06-26 11:58:59 +0545

    refactor: update variable

--- packet_analyzer/communication.py
+++ packet_analyzer/communication.py
@@ -3,11 +3,27 @@ import websockets
 
 
 class Communication:
-    @staticmethod
-    async def communicate(data):
-        print(data)
+    # def __init__(self, uri):
+    #     self.uri = uri
+    #     self.websocket = None
+    #
+    # async def connect(self):
+    #     self.websocket = await websockets.connect(self.uri)
+    #
+    # async def disconnect(self):
+    #     await self.websocket.close()
+    #     self.websocket = None
+    #
+    # async def communicate(self, data):
+    #     if self.websocket is None:
+    #         raise RuntimeError("WebSocket connection is not established.")
+    #     await self.websocket.send(json.dumps({'message': data}))
+    #     response = await self.websocket.recv()
+    #     print(response)
+
+    async def communicate(self, data):
         uri = "ws://localhost:8000/ws/flow_data/"
         async with websockets.connect(uri) as websocket:
-            await websocket.send(json.dumps({'message': data}))
+            await websocket.send(json.dumps({'data': data}))
             response = await websocket.recv()
             print(response)
